02199468e9ebdee57d1899df54bb54ad074b64c3
CondoriWilliam <wcondorian@unsa.edu.pe>
2024-10-16 21:29:54 -0700

    Initial Commit

---
+++ PaginationAPI/PaginationAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaginationAPIApp.swift
+//  PaginationAPI
+//
+//  Created by galaxy on 16/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PaginationAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
